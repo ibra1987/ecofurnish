5e3915837ba5812217609fe04409a0d069364d1d
ibra1987 <moi-meme10@hotmail.com>
2024-02-02 17:47:35 +0100

    fix quill editor export

--- app/app/dashboard/dashboardComponents/RichTextEditor.tsx
+++ app/app/dashboard/dashboardComponents/RichTextEditor.tsx
@@ -5,15 +5,17 @@ import { forwardRef } from 'react';
 import ReactQuill, { ReactQuillProps } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
-const RichTextEditor = dynamic(
-  async () => {
-    const { default: RQ } = await import('react-quill');
-    // Forward the ref with the correct type
-    return forwardRef<ReactQuill, ReactQuillProps>((props, ref) => <RQ ref={ref} {...props} />);
-  },
-  {
-    ssr: false,
-  }
-) as typeof ReactQuill;
+export default function RichTextEditor (){
 
-export { RichTextEditor };
+  return dynamic(
+    async () => {
+      const { default: RQ } = await import('react-quill');
+      // Forward the ref with the correct type
+      return forwardRef<ReactQuill, ReactQuillProps>((props, ref) => <RQ ref={ref} {...props} />);
+    },
+    {
+      ssr: false,
+    }
+  )as typeof ReactQuill;
+  }
+
